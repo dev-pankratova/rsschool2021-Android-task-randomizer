4809809671cd31d9fa23a2e63f1ac9b03dd39211
Olesya Pankratova <pan.develop89@gmail.com>
2021-05-31 01:54:25 +0600

    feat: add transaction commit

--- app/src/main/java/com/rsschool/android2021/MainActivity.java
+++ app/src/main/java/com/rsschool/android2021/MainActivity.java
@@ -1,6 +1,9 @@
 package com.rsschool.android2021;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,22 +11,36 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    Button generateBtn;
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         openFirstFragment(0);
+
     }
 
     private void openFirstFragment(int previousNumber) {
         final Fragment firstFragment = FirstFragment.newInstance(previousNumber);
-        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, firstFragment);
-        // TODO: invoke function which apply changes of the transaction
+        transaction.commit();
+        generateBtn = firstFragment.getView().findViewById(R.id.generate);
+        TextView minTxt = firstFragment.getView().findViewById(R.id.min_value);
+        TextView maxTxt = firstFragment.getView().findViewById(R.id.max_value);
+
+        openSecondFragment(Integer.parseInt(minTxt.getText().toString()), Integer.parseInt(maxTxt.getText().toString()));
     }
 
     private void openSecondFragment(int min, int max) {
-        // TODO: implement it
+        final Fragment secondFragment = SecondFragment.newInstance(min, max);
+        generateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transaction.replace(R.id.container, secondFragment);
+            }
+        });
     }
 }
